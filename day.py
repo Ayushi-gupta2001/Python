d7eb06e911888d7b762a4c67bc19cbb3e512c2ab
Ayushi gupta <ayushigupta@localhost.localdomain>
2022-01-06 01:06:17 -0500

    Our first section python file

---
+++ day.py
@@ -0,0 +1,26 @@
+########################################
+
+########   Writing first programe ##################
+
+print(24*5)
+day_hour = 24 
+week_hour = 67
+print( day_hour * week_hour)
+
+
+######## Integer , float, string #########
+
+x = 10
+y = '10'
+sum = x+x
+sum1= y+y
+print(sum, sum1)
+print(type(sum1))
+
+
+######## List types ###############
+
+student_grade = [2,3,4,5,6]
+print(type(student_grade))
+print((student_grade)/2)
+
