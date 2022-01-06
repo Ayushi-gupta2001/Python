b79431458e4e892533bcf1150d20f2fd46dab813
Ayushi gupta <g.dibs2001@gmail.com>
2022-01-06 06:30:55 -0500

    Adding some tuple dic list

--- day.py
+++ day.py
@@ -20,13 +20,43 @@ print(type(sum1))
 
 ######## List types ###############
 
-student_grade = [2,3,4,5,6]
-print(type(student_grade))
+student_grade2 = [2,3,4,5,6]
+print(type(student_grade2))
 
 
 ######### Ranges in list ###########
 
-student_grade = list(range(1,10))
+student_grade1 = list(range(1,10))
+print(student_grade1)
+
+
+#########################
+
+###########  Average value #########
+student_grade = [1,2,3,2,4]
+
+#total_price = sum(student_grade)
+#total_len = len(student_grade)
+#print(type(total_price))
+#print(total_price / total_len)
+
+####### Count, lower function  ##########
+
+print("hello".lower())
+print(student_grade.count(2))
+
+
+####### Dictoniry ######
+
+monday_tem = [2.5, 4.6,7.8]
+assign_per = {"Harry":2.5, "AYushi":4.6, "GUpta":7.8}
+
+print(assign_per.values())
+print(assign_per.keys())
+
+#print(sum(assign_per.values()))
+
 print(student_grade)
 
-print("Hello")
+tuple = (1,2,3)
+print(tuple)
